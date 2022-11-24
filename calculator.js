5606e2e70cf3d61ae5e0be75da48aa5f3ef9b4a3
kubisense <87003603+kubisense@users.noreply.github.com>
2022-11-24 17:28:02 +0000

    updated calculator.js to display more than one input on screen

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
+let x=0;
 function Button(i){
 
     //console.log(buttonPress.length);
     let displayScreen = document.getElementById('display-screen');
-        displayScreen.innerHTML = parseInt(i);
-        console.log(i);
 
+    //read displayscreen content into a variable x
+    x =  displayScreen.innerHTML;
+
+    // add next input (number in the form of string) to stored x value and output on display screen
+    displayScreen.innerHTML = x + i;
+        
 }
 
