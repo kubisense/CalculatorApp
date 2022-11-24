1bbf8c3e5b149aa59dad1229733fd22ff403952b
kubisense <87003603+kubisense@users.noreply.github.com>
2022-11-24 16:42:52 +0000

    My first commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function Button(i){
+
+    //console.log(buttonPress.length);
+    let displayScreen = document.getElementById('display-screen');
+        displayScreen.innerHTML = parseInt(i);
+        console.log(i);
+
+}
+
